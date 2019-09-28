ee8d64eaf259ce1569e03fff252409ad196406de
scottpwnstheworld <Thaddeusjcurry@gmail.com>
2019-09-27 18:32:21 -0700

    Added scenery to first tutorial level. Added enemy proximity detector for the player

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour {
 
-
+    public GameObject Player;
     public float speed;
-
+    public float distance;
     private Transform target;
 	// Use this for initialization
 	void Start ()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
 	}
-	
-	// Update is called once per frame
-	void Update ()
+   
+    // Update is called once per frame
+    void Update ()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        
+        if (Mathf.Abs(this.transform.position.x - Player.transform.position.x) <= distance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+       
 	}
 }

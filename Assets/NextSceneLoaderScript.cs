07f24e04ba61fc55537ef65df8f7ec6bb5a8febb
scottpwnstheworld <Thaddeusjcurry@gmail.com>
2019-09-29 14:56:06 -0700

    Finished first tutorial level and first cutscene, working on transitions between scenes

---
+++ Assets/NextSceneLoaderScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextSceneLoaderScript : MonoBehaviour {
+
+   int nextSceneToLoad;
+	
+	void Start ()
+    {
+
+        nextSceneToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+	}
+	
+    void onTriggerEnter2D(Collider2D col)
+    {
+        if(col.tag == "Player")
+        {
+            Debug.Log("NExtScene");
+            SceneManager.LoadScene(nextSceneToLoad);
+            Debug.Log("NextSCene");
+        }
+            
+        
+    }
+   public void nextScene()
+    {
+        SceneManager.LoadScene(nextSceneToLoad);
+    }
+}
--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class EnemyFollow : MonoBehaviour {
 
-    public GameObject Player;
+  
     public float speed;
     public float distance;
     private Transform target;
 	// Use this for initialization
 	void Start ()
     {
+        Debug.Log("findingtarget");
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        Debug.Log(target);
 	}
+
+
    
     // Update is called once per frame
     void Update ()
     {
-        
-        if (Mathf.Abs(this.transform.position.x - Player.transform.position.x) <= distance)
+        if (Mathf.Abs(this.transform.position.x - target.position.x) <= distance)
         {
+            Debug.Log("Goingtoplayer");
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
         }
        

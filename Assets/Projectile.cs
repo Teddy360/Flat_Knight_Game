f6a66cf37f0988a129d929054cea2486c5a45bea
scottpwnstheworld <Thaddeusjcurry@gmail.com>
2019-12-13 18:28:05 -0800

    Continued attempting to add projectiles

---
+++ Assets/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    public Transform firepoint;
+    public GameObject Rock;
+    public int ShotPower;
+	void OnTriggerEnter2D(Collider2D col)
+    {
+       
+        if (col.tag == "Floor")
+        {
+            
+            Shoot();
+        }
+    }
+	void Shoot()
+    {
+        print("SHOOT SOMETHING");
+        Instantiate(Rock,new Vector3(firepoint.position.x, firepoint.position.y, firepoint.position.z), Quaternion.identity);
+        //this.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(ShotPower, 0));
+        /*Vector2 v = -transform.forward;
+        Rock.GetComponent<Rigidbody2D>().AddForce(v * 100f); */
+
+    }
+}
---
+++ Assets/RockShoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockShoot : MonoBehaviour {
+    
+    public int ShotPower;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+    void FixedUpdate()
+    { 
+       rb.AddForce(transform.forward * -100f);
+        print("");
+    }
+}
